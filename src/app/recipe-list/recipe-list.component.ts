77f3a62b0fb267e05b22d78ed8d04c85595da66b
N4de <jaAnnala@gmail.com>
2020-02-16 19:41:50 +0200

    add recipe-list component

---
+++ src/app/recipe-list/recipe-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+import { recipes } from '../recipes';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes = recipes;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
