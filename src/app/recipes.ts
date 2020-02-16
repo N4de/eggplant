6010a5df26192b7bc4e9527a2f09cb4d0cfd3ca8
N4de <jaAnnala@gmail.com>
2020-02-16 19:41:08 +0200

    add mock recipes

---
+++ src/app/recipes.ts
@@ -0,0 +1,58 @@
+export const recipes = [
+  {
+    name: 'Beef Stew',
+    incredients: [
+      {
+          name: 'carrot',
+          amount: '500g'
+      },
+      {
+        name: 'cucumber',
+        amount: '565g'
+      },
+      {
+          name: 'milk',
+          amount: '45ml'
+      }
+    ],
+    description: 'A large stew with beef'
+  },
+
+  {
+    name: 'Apple pie',
+    incredients: [
+      {
+          name: 'apple',
+          amount: '500g'
+      },
+      {
+        name: 'pie',
+        amount: '565g'
+      },
+      {
+          name: 'milk',
+          amount: '45ml'
+      }
+    ],
+    description: 'A large pie with apple'
+  },
+
+  {
+    name: 'banana split',
+    incredients: [
+      {
+          name: 'banana',
+          amount: '500g'
+      },
+      {
+        name: 'ice cream',
+        amount: '43oz'
+      },
+      {
+          name: 'water',
+          amount: '45ml'
+      }
+    ],
+    description: 'A large banana with split'
+  },
+];
